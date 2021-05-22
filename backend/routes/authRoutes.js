a3226c8f4da85896dee4eec3bc37d1b8b253e8c3
Tejas Srikanth <tejas.c.srikanth.2020@outlook.com>
2021-05-22 17:04:18 -0400

    add auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-const User = mongoose.model('../models/User')
+const User = require('../models/User')
 
 const router = express.Router()
 
