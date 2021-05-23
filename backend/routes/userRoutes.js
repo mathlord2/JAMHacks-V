5ae4d0d66ce6fbd8a6ae1149fa34af67b33d5c2d
Tejas Srikanth <tejas.c.srikanth.2020@outlook.com>
2021-05-23 02:15:23 -0400

    user ads

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ var multer = require('multer');
 var upload = multer({ dest: 'uploads/' })
 var fs = require('fs');
 const User = mongoose.model('User')
+const Ad = mongoose.model('Ad')
 
 const router = express()
 router.use(requireAuth)
@@ -16,6 +17,16 @@ router.get('/users/:userID', async (req, res) => {
         res.send(user)
     } catch (e) {
         res.send({error: e.message})
+    }
+})
+
+//get a user's ads
+router.get('/users/:userID/ads', async (req, res) => {
+    try{
+        const ads = Ad.find({owner: req.params.userID})
+        res.send(ads)
+    } catch(e){
+        console.log({error: e.message})
     }
 })
 
